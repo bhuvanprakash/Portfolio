60d1182257aee5b64234ff7a2629bfb4d4b40bcc
Bhuvan Prakash <52481324+bhuvanprakash@users.noreply.github.com>
2024-06-21 17:55:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function runTypingEffect() {
-    const text = "I am Asim Abbasi.";
+    const text = "I am Bhuvan Prakash.";
     const typingElement = document.getElementById("typing-text");
     const typingDelay = 100;
 
